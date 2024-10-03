c879ef2e9c3084f1c5be8424a12854d6f41721e3
antonio-sgi <antonioagile2024@gmail.com>
2024-10-03 20:25:04 +0200

    introduccion al control de flujo y operadores

--- src/main/java/com/example/Persona.java
+++ src/main/java/com/example/Persona.java
@@ -2,15 +2,15 @@ package com.example;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 @Builder
+@Data
 public class Persona {
 
     private String nombre;
@@ -20,4 +20,9 @@ public class Persona {
     private double salario;
 
 
-}
+    }
+
+    
+
+
+
